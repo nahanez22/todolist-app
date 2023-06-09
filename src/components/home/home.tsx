7b90495886bcc5047880547075a4f78c959ac4ed
Nahum Añez <nahumatencio@gmail.com>
2023-06-09 10:38:31 -0500

    Add feature  add task

--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -1,13 +1,32 @@
+"use client";
+
+import { useState, Dispatch, SetStateAction } from "react";
 import AddTasks from "../tasks/addTasks";
 import Header from "../tasks/header";
 import TaskList from "../tasks/taskList";
 
+interface Task {
+  title: string;
+  isComplete: boolean;
+}
+
 const Home = () => {
+  const [tasks, setTask] = useState<Task[]>([]);
+
+  const addNewTask = (name: string) => {
+    const newTask: Task = {
+      title: name,
+      isComplete: false,
+    };
+
+    setTask((oldTasks) => [newTask, ...oldTasks]);
+  };
+
   return (
     <main className="flex min-h-screen flex-col items-center  p-24">
       <Header />
       <div className="mt-12 md:flex gap-4">
-        <AddTasks />
+        <AddTasks addNewTask={addNewTask} />
         <TaskList />
       </div>
     </main>
--- src/components/tasks/taskList.tsx
+++ src/components/tasks/taskList.tsx
@@ -1,9 +1,16 @@
-import React from "react";
+import { useState } from "react";
 
 function TaskList() {
+  const [count, setCount] = useState(0);
+
   return (
     <main className="md:w-1/2 lg:w-3/5 md:h-screen ">
       <h2 className="text-lg mt-4 text-center mb-8">Listado de tareas</h2>
+
+      <div>
+        <p>You clicked {count} times</p>
+        <button onClick={() => setCount(count + 1)}>Click me</button>
+      </div>
     </main>
   );
 }
